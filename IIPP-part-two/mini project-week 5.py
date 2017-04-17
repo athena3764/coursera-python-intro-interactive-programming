f7c0b2fc63d25cda2e5c148ec329c981c916f4f9
jhhsu8 <jhhsu11@gmail.com>
2017-04-17 14:15:28 -0700

    Update mini project-week 5.py

--- IIPP-part-two/mini project-week 5.py
+++ IIPP-part-two/mini project-week 5.py
@@ -3,9 +3,9 @@ import simplegui,random
 
 # helper function to initialize globals
 def new_game():
-    global state, card1, card2, card, card_cover, count
+    global state, card1, card2, card, card_flipped, count
     card = list(range(1,9) + range(1,9))
-    card_cover = list(range(1,9) + range(1,9))
+    card_flipped = list(range(1,9) + range(1,9))
     state = 0
     card1 = 0
     card2 = 0
@@ -15,40 +15,49 @@ def new_game():
 
 # define event handlers
 def mouseclick(pos):
-    global state, card1, card2, card, card_cover, count
+    global state, card1, card2, card, card_flipped, count
+    
     #compute the index of the card
     index = pos[0]//50
+    
     #flip current card, save it as the first card, next state is 1
     if state == 0:
         state = 1
         card1 = index 
-        card_cover[card1] = False
+        card_flipped[card1] = False
         count +=1
+    
     #flip current card, save it as the second card, next state is 2
     elif state == 1:
         state = 2
         card2 = index 
-        card_cover[card2] = False
-    #check if the first and second cards match, and if not unflip them.
-    #Regardless of what happened in that check, flip current card, save it as the first card next state is 1
+        card_flipped[card2] = False
+    
+    #check if the first and second cards match, and if not, unflip 
+    #them. Regardless of what happened in that check, flip current 
+    #card, save it as the first card and next state is 1
     else:
         state = 1
         if card[card1] != card[card2]:
-            card_cover[card2] = True
-            card_cover[card1] = True
+            card_flipped[card2] = True
+            card_flipped[card1] = True
         card1 = index 
-        card_cover[card1] = False
+        card_flipped[card1] = False
         count +=1
+    
     #total count of turns
     label.set_text("Turns = " + str(count))
-   
+    
+    
 def draw(canvas):
-    global card, card_cover
+    global card, card_flipped
     for card_index in range(len(card)):
         card_pos = 50*card_index
+        
         #draw exposed cards
-        if card_cover[card_index] == False:
+        if card_flipped[card_index] == False:
             canvas.draw_text(str(card[card_index]),(card_pos+25,50), 30, "White")
+        
         #draw flipped cards
         else:
             canvas.draw_polygon([[card_pos, 0],[card_pos, 100],[card_pos+50, 100],[card_pos+50,0]], 1, "Red", "Green") 
